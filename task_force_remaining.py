a8a54fe3683dd8203d7bf707ff25d2b7dc2a7014
Dalai Felinto <dfelinto@gmail.com>
2018-04-30 15:01:47 +0200

    Get conduit API from cli

--- task_force_remaining.py
+++ task_force_remaining.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 
-# input variables
-conduit_api_key = '/home/dfelinto/.conduit-dev.b.o'
-
 import requests
 manifest_url = "https://developer.blender.org/api/maniphest.info"
 phid_url = "https://developer.blender.org/api/phid.lookup"
 
-
 # Settings required for all the requests calls.
-api_token = open(conduit_api_key).read().rstrip()
 headers = {'Content-Type': 'application/json'}
 
-
-def get_tasks(task_id):
+def get_tasks(api_token, task_id):
     """
     Return all the children tasks ids for a task id
     """
@@ -43,7 +37,7 @@ def get_tasks(task_id):
     return tasks
 
 
-def get_tasks_content(tasks):
+def get_tasks_content(api_token, tasks):
     """
     Return the raw content of given tasks
     """
@@ -80,30 +74,39 @@ def extract_info(raw_text):
     return count_done, count_all
 
 
-def get_main_task_id():
+def parse_arguments():
     """
     Parse input
-    We get the main task id from command-line.
+    We get the main task id from command-line and the conduit API.
     """
     import argparse
+
     parser = argparse.ArgumentParser("task_force_remaining")
     parser.add_argument("task_id", help=
     "The task number for the task force (e.g., 54641 for multi-object or 54810 for copy-on-write)"
     , type=int)
+    parser.add_argument("conduit_api_key", help=
+    "Certificate token obtain from https://developer.blender.org/conduit/token"
+    , type=str)
     args = parser.parse_args()
 
-    return args.task_id
+    return args.task_id, args.conduit_api_key
 
 
 def main():
     """
     Return (print) the remaining tasks for a given task-force.
     """
-    main_task_id = get_main_task_id()
-    tasks = get_tasks(main_task_id)
-    content = get_tasks_content(tasks)
+    main_task_id, api_key = parse_arguments()
+
+    # Require connection.
+    tasks = get_tasks(api_key, main_task_id)
+    content = get_tasks_content(api_key, tasks)
+
+    # Process the obtained info.
     count_done, count_all = extract_info(content)
 
+    # Return the final result.
     remaining_tasks = count_all - count_done
     print(remaining_tasks)
 
